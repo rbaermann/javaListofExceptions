cb67eaceaf78512cdd83299e8e2d95774902eeb7
Ryan Baermann <rcbaermann@gmail.com>
2019-10-01 15:33:54 -0700

    initial commit

---
+++ Exceptions.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class Exceptions {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        for(int i = 0; i < myList.size(); i++) {
+            try {
+                Integer castedValue = (Integer) myList.get(i);
+            }
+            catch(Exception e) {
+                System.out.println(myList.get(i) + " is not an Integer");
+            }
+        }
+    }
+}
